148356ab1c894b9fcf5e56cfd3490a62ecdcc8b2
Joongi Kim <joongi@lablup.com>
2018-11-24 03:30:43 +0900

    Code clean up

--- src/ai/backend/client/__init__.py
+++ src/ai/backend/client/__init__.py
@@ -1,9 +1,9 @@
 from .exceptions import *  # noqa
-#from .session import *  # noqa
+from .session import *  # noqa
 
 __all__ = [
     exceptions.__all__,  # noqa
-    #session.__all__,  # noqa
+    session.__all__,  # noqa
 ]
 
 __version__ = '1.4.2'
