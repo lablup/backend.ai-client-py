ffa414029806d30c2e3030a52542ba465e0a65ac
Kyujin Cho <kyujin.cho@lablup.com>
2019-11-14 16:14:56 +0900

    Apply new argument parsing method

--- src/ai/backend/client/cli/app.py
+++ src/ai/backend/client/cli/app.py
@@ -1,7 +1,8 @@
 import asyncio
 import json
+import shlex
 import signal
-from typing import MutableMapping
+from typing import MutableMapping, Union, List
 
 import aiohttp
 import click
@@ -27,7 +28,7 @@ class WSProxy:
                  session_id: str,
                  app_name: str,
                  protocol: str,
-                 args: MutableMapping[str, str], 
+                 args: MutableMapping[str, Union[None, str, List[str]]], 
                  envs: MutableMapping[str, str],
                  reader: asyncio.StreamReader,
                  writer: asyncio.StreamWriter):
@@ -158,9 +159,7 @@ class ProxyRunner:
               help='The IP/host address and the port number to bind this proxy.')
 @click.option('--arg', type=str, multiple=True, metavar='"--option <value>"', help='Add additional argument when starting service.')
 @click.option('--env', type=str, multiple=True, metavar='"ENVNAME=envvalue"', help='Add additional environment variable when starting service.')
-@click.option('--show-configurables', is_flag=True, 
-              help='List available additional arguments and environment variables when starting service.')
-def app(session_id, app, protocol, bind, arg, env, show_configurables):
+def app(session_id, app, protocol, bind, arg, env):
     """
     Run a local proxy to a service provided by Backend.AI compute sessions.
 
@@ -172,30 +171,6 @@ def app(session_id, app, protocol, bind, arg, env, show_configurables):
     """
     api_session = None
     runner = None
-
-    async def print_arguments():
-        nonlocal api_session
-        api_session = AsyncSession()
-        path = "/stream/kernel/{0}/apps".format(session_id)
-        api_rqst = Request(
-            api_session, "GET", path, b'',
-            params={'app': app},
-            content_type="application/json")
-        try:
-            async with api_rqst.fetch() as resp:
-                data = await resp.json()
-                if 'allowed_arguments' in data.keys():
-                    print_info('Available arguments: {0}'.format(data['allowed_arguments']))
-                if 'allowed_envs' in data.keys():
-                    print_info('Available environment variables: {0}'.format(data['allowed_envs']))
-            await api_session.close()
-        except:
-            pass
-
-    if show_configurables:
-        loop = current_loop()
-        loop.run_until_complete(print_arguments())
-        return 
 
     bind_parts = bind.rsplit(':', maxsplit=1)
     if len(bind_parts) == 1:
@@ -210,14 +185,25 @@ def app(session_id, app, protocol, bind, arg, env, show_configurables):
         loop = current_loop()
         api_session = AsyncSession()
         arguments, envs = {}, {}
+
         if len(arg) > 0:
             for argline in arg:
-                split = argline.strip().split(' ', maxsplit=2)
-                if len(split) == 2:
-                    arguments[split[0]] = split[1]
+                tokens = []
+                for token in shlex.shlex(argline, punctuation_chars=True):
+                    kv = token.split('=', maxsplit=1)
+                    if len(kv) == 1:
+                        tokens.append(shlex.split(token)[0])
+                    else:
+                        tokens.append(kv[0])
+                        tokens.append(shlex.split(kv[1])[0])
+
+                if len(tokens) == 1:
+                    arguments[tokens[0]] = None
+                elif len(tokens) == 2:
+                    arguments[tokens[0]] = tokens[1]
                 else:
-                    arguments[split[0]] = ''
-        
+                    arguments[tokens[0]] = tokens[1:]
+
         if len(env) > 0:
             for envline in env:
                 split = envline.strip().split('=', maxsplit=2)
@@ -269,3 +255,38 @@ def app(session_id, app, protocol, bind, arg, env, show_configurables):
 
     asyncio_run_forever(app_setup(), app_shutdown(),
                         stop_signals={signal.SIGINT, signal.SIGTERM})
+
+
+@main.command()
+@click.argument('session_id', type=str, metavar='SESSID')
+@click.argument('app', type=str)
+def apps(session_id, app):
+    '''
+    List available additional arguments and environment variables when starting service.
+
+    \b
+    SESSID: The compute session ID.
+    APP: The name of service provided by the given session.
+    '''
+    
+    async def print_arguments():
+        api_session = AsyncSession()
+        path = "/stream/kernel/{0}/apps".format(session_id)
+        api_rqst = Request(
+            api_session, "GET", path, b'',
+            params={'app': app},
+            content_type="application/json")
+        try:
+            async with api_rqst.fetch() as resp:
+                data = await resp.json()
+                if 'allowed_arguments' in data.keys():
+                    print_info('Available arguments: {0}'.format(data['allowed_arguments']))
+                if 'allowed_envs' in data.keys():
+                    print_info('Available environment variables: {0}'.format(data['allowed_envs']))
+        except Exception as e:
+            print(e)
+        finally:
+            await api_session.close()
+
+    loop = current_loop()
+    loop.run_until_complete(print_arguments())
