00525a466bedc488d7b26b3ab9e4f722cafd4808
Jonghyun Park <adrysn@gmail.com>
2020-04-20 11:11:03 +0900

    fix: remove debug script

--- tests/__init__.py
+++
@@ -1,7 +0,0 @@
-import random
-import string
-
-
-def random_string_with_digits(length=6):
-    allowed_choices = string.ascii_letters + string.digits
-    return ''.join(random.choice(allowed_choices) for i in range(length))
