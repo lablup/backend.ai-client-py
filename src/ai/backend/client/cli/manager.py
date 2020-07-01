fc1a1582831160b80f143ab32636f9b5a32399d1
Joongi Kim <joongi@lablup.com>
2020-07-01 16:16:34 +0900

    fix: Handle errors gracefully and pretty

--- src/ai/backend/client/cli/manager.py
+++ src/ai/backend/client/cli/manager.py
@@ -9,24 +9,27 @@ from tabulate import tabulate
 
 from . import main
 from .interaction import ask_yn
-from .pretty import print_done, print_fail, print_info, print_wait
+from .pretty import print_done, print_error, print_fail, print_info, print_wait
 from ..session import Session
 
 
 @main.group()
 def manager():
     '''Provides manager-related operations.'''
-    print('Run with -h/--help for usage.')
 
 
 @manager.command()
 def status():
     '''Show the manager's current status.'''
-    with Session() as session:
-        resp = session.Manager.status()
-        print(tabulate([('Status', 'Active Sessions'),
-                        (resp['status'], resp['active_sessions'])],
-                       headers='firstrow'))
+    try:
+        with Session() as session:
+            resp = session.Manager.status()
+            print(tabulate([('Status', 'Active Sessions'),
+                            (resp['status'], resp['active_sessions'])],
+                           headers='firstrow'))
+    except Exception as e:
+        print_error(e)
+        sys.exit(1)
 
 
 @manager.command()
@@ -41,36 +44,43 @@ def freeze(wait, force_kill):
         print('You cannot use both --wait and --force-kill options '
               'at the same time.', file=sys.stderr)
         return
+    try:
+        with Session() as session:
+            if wait:
+                while True:
+                    resp = session.Manager.status()
+                    active_sessions_num = resp['active_sessions']
+                    if active_sessions_num == 0:
+                        break
+                    print_wait('Waiting for all sessions terminated... ({0} left)'
+                               .format(active_sessions_num))
+                    time.sleep(3)
+                print_done('All sessions are terminated.')
 
-    with Session() as session:
-        if wait:
-            while True:
-                resp = session.Manager.status()
-                active_sessions_num = resp['active_sessions']
-                if active_sessions_num == 0:
-                    break
-                print_wait('Waiting for all sessions terminated... ({0} left)'
-                           .format(active_sessions_num))
-                time.sleep(3)
-            print_done('All sessions are terminated.')
+            if force_kill:
+                print_wait('Killing all sessions...')
 
-        if force_kill:
-            print_wait('Killing all sessions...')
+            session.Manager.freeze(force_kill=force_kill)
 
-        session.Manager.freeze(force_kill=force_kill)
+            if force_kill:
+                print_done('All sessions are killed.')
 
-        if force_kill:
-            print_done('All sessions are killed.')
-
-        print('Manager is successfully frozen.')
+            print('Manager is successfully frozen.')
+    except Exception as e:
+        print_error(e)
+        sys.exit(1)
 
 
 @manager.command()
 def unfreeze():
     '''Unfreeze manager.'''
-    with Session() as session:
-        session.Manager.unfreeze()
-        print('Manager is successfully unfrozen.')
+    try:
+        with Session() as session:
+            session.Manager.unfreeze()
+            print('Manager is successfully unfrozen.')
+    except Exception as e:
+        print_error(e)
+        sys.exit(1)
 
 
 @main.group()
@@ -81,13 +91,17 @@ def announcement():
 @announcement.command()
 def get():
     '''Get current announcement.'''
-    with Session() as session:
-        result = session.Manager.get_announcement()
-        if result.get('enabled', False):
-            msg = result.get('message')
-            print(msg)
-        else:
-            print('No announcements.')
+    try:
+        with Session() as session:
+            result = session.Manager.get_announcement()
+            if result.get('enabled', False):
+                msg = result.get('message')
+                print(msg)
+            else:
+                print('No announcements.')
+    except Exception as e:
+        print_error(e)
+        sys.exit(1)
 
 
 @announcement.command()
@@ -98,14 +112,20 @@ def update(message):
 
     MESSAGE: Announcement message.
     '''
-    with Session() as session:
-        if message is None:
-            message = click.edit("<!-- Use Markdown format to edit the announcement message -->")
-        if message is None:
-            print_info('Cancelled')
-            sys.exit(1)
-        session.Manager.update_announcement(enabled=True, message=message)
-    print_done('Posted new announcement.')
+    try:
+        with Session() as session:
+            if message is None:
+                message = click.edit(
+                    "<!-- Use Markdown format to edit the announcement message -->"
+                )
+            if message is None:
+                print_info('Cancelled')
+                sys.exit(1)
+            session.Manager.update_announcement(enabled=True, message=message)
+        print_done('Posted new announcement.')
+    except Exception as e:
+        print_error(e)
+        sys.exit(1)
 
 
 @announcement.command()
@@ -114,9 +134,13 @@ def delete():
     if not ask_yn():
         print_info('Cancelled.')
         sys.exit(1)
-    with Session() as session:
-        session.Manager.update_announcement(enabled=False)
-    print_done('Deleted announcement.')
+    try:
+        with Session() as session:
+            session.Manager.update_announcement(enabled=False)
+        print_done('Deleted announcement.')
+    except Exception as e:
+        print_error(e)
+        sys.exit(1)
 
 
 @announcement.command()
@@ -136,6 +160,9 @@ def dismiss():
     except (IOError, json.JSONDecodeError):
         print_fail('No announcements seen yet.')
         sys.exit(1)
+    except Exception as e:
+        print_error(e)
+        sys.exit(1)
 
 
 @manager.group()
@@ -153,9 +180,13 @@ def include_agents(agent_ids):
     Include agents in scheduling, meaning that the given agents
     will be considered to be ready for creating new session containers.
     """
-    with Session() as session:
-        session.Manager.scheduler_op('include-agents', agent_ids)
-    print_done('The given agents now accepts new sessions.')
+    try:
+        with Session() as session:
+            session.Manager.scheduler_op('include-agents', agent_ids)
+        print_done('The given agents now accepts new sessions.')
+    except Exception as e:
+        print_error(e)
+        sys.exit(1)
 
 
 @scheduler.command()
@@ -166,6 +197,10 @@ def exclude_agents(agent_ids):
     will no longer start new sessions unless they are "included" again,
     regardless of their restarts and rejoining events.
     """
-    with Session() as session:
-        session.Manager.scheduler_op('exclude-agents', agent_ids)
-    print_done('The given agents will no longer start new sessions.')
+    try:
+        with Session() as session:
+            session.Manager.scheduler_op('exclude-agents', agent_ids)
+        print_done('The given agents will no longer start new sessions.')
+    except Exception as e:
+        print_error(e)
+        sys.exit(1)
