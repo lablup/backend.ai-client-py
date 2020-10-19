3081432f6112e71e8ab7fdf86144200889836d5f
Sergey Leksikov <lexikovs@gmail.com>
2020-10-19 21:28:17 +0900

    feat: scp complete file upload/download

--- src/ai/backend/client/cli/scp.py
+++ src/ai/backend/client/cli/scp.py
@@ -4,41 +4,46 @@ from . import main
 
 
 @main.command()
-@click.argument("sess_name",  type=str, metavar='USER')
-@click.option('-p', '--port',  type=int, metavar='PORT', default=9922, help="host port number")
-@click.option('-c', '--command', type=str, metavar="COMMAND", help="command to copy files")
-def scp(sess_name, port, command):
-    """SSH client to SSH server.
+@click.argument("sess_name",  type=str, metavar='SESS_NAME')
+@click.argument("src",  type=str, metavar='SRC')
+@click.argument("dest",  type=str, default="work@localhost", metavar='DEST')
+@click.option('-p', '--port',  type=str, metavar='PORT', default=9922,
+              help="host port number")
+@click.option('-r', '--reverse',  metavar='REVERSE', default=False, is_flag=True,
+              help="option to reverse copy from server to local")
+def scp(sess_name, src, dest, port, reverse):
+    """SCP client to Backend.AI SSH server.
 
     \b
-    USER: Name of the user at the session image. Default: work
-    HOST: ip address of the host machine. Default: localhost or 127.0.0.1
-    ID_PATH: path to the id_container which contains private key of the remote host.
-    Default ~/.ssh/id_container
-    PORT: port number of remote host SSH server.
+    It is assumed that first connection is made through backend.ai ssh which
+     starts SSH Server.
+
+    SESS_NAME: Name of the running session.
+    SRC: file which you want to move
+    Dest: ip address of the host machine and directory. work@localhost:tmp/
+    Port: Port num where ssh is opened
+    Reverse: Flag to download file from server to directory
+
+    Full example to download file: backend.ai mysess  vfolder.py work@localhost:tmp/ -p 9922 -r
     """
+
     id_path = "~/.ssh/id_container"
-    user = "work"
-    host = "localhost"
 
-    popen = subprocess.Popen(["backend.ai", "download", "{}".format(sess_name),
-                              "id_container"], shell=False)
-    popen.communicate()
-    popen = subprocess.Popen(["mv", "id_container", "~/.ssh/"], shell=False)
-    popen.communicate()
-    popen = subprocess.Popen(["backend.ai", "app", "{}".format(sess_name),
-                              "sshd", "-b", "{}".format(port)], shell=True)
-    popen.communicate()
+    info_str = "session name: {}; src: {}; dest: {}; port: {}".format(
+                sess_name, src, dest, port)
 
-    info_str = "session name: {}; user: {}; server: {}; port: {}".format(
-                sess_name, user, host, port)
-    popen = subprocess.Popen(["echo",
-                             "\n****************\n{}\n****************"
-                              .format(info_str)], shell=False)
+    subprocess.Popen(["echo",
+                      "\n****************\n{}; \n****************"
+                      .format(info_str)], shell=False)
 
-    scp = subprocess.Popen(["scp", "-o", "StrictHostKeyChecking=no", "-i",
-                            id_path, "-P", port, command], shell=False)
-    scp.communicate()
-    # scp -o StrictHostKeyChecking=no -i id_container -P 9922 vfolder.py work@localhost:tmp/
+    if not reverse:
+        scp_proc = subprocess.Popen(["scp", "-o", "StrictHostKeyChecking=no", "-i",
+                                    id_path, "-P", str(port), src, dest],
+                                    shell=False)
+    else:
+        scp_proc = subprocess.Popen(["scp", "-o", "StrictHostKeyChecking=no", "-i",
+                                     id_path, "-P", str(port), dest, src],
+                                    shell=False)
+    scp_proc.communicate()
     print("Done")
     return 1
