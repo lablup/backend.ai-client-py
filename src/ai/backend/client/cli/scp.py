57aecea47c81215e2ddcc8ec75fad8f5b047582e
Sergey Leksikov <lexikovs@gmail.com>
2020-10-20 13:03:35 +0900

    feat: SCP support for upload/download directories

--- src/ai/backend/client/cli/scp.py
+++ src/ai/backend/client/cli/scp.py
@@ -9,41 +9,36 @@ from . import main
 @click.argument("dest",  type=str, default="work@localhost", metavar='DEST')
 @click.option('-p', '--port',  type=str, metavar='PORT', default=9922,
               help="host port number")
-@click.option('-r', '--reverse',  metavar='REVERSE', default=False, is_flag=True,
-              help="option to reverse copy from server to local")
-def scp(sess_name, src, dest, port, reverse):
+@click.option('-r',  default=False, is_flag=True, help="option to process directories")
+def scp(sess_name, src, dest, port, r):
     """SCP client to Backend.AI SSH server.
 
     \b
-    It is assumed that first connection is made through backend.ai ssh which
-     starts SSH Server.
+    It is assumed that first connection is amde through backend.ai ssh which starts SSH Server.
 
     SESS_NAME: Name of the running session.
-    SRC: file which you want to move
-    Dest: ip address of the host machine and directory. work@localhost:tmp/
+    SRC: file or directory which you want to move
+    Dest: file or directory destination. Ex) work@localhost:tmp/
     Port: Port num where ssh is opened
-    Reverse: Flag to download file from server to directory
+    R: Option to download/ upload directories
 
-    Full example to download file: backend.ai mysess  vfolder.py work@localhost:tmp/ -p 9922 -r
+    Example: upload local directory to remote host: backend.ai scp mysess -p 9922 -r tmp/ work@localhost:tmp2/
+    download directory from host to local: backend.ai scp mysess -p 9922 -r work@localhost:tmp2/ tmp/
     """
 
     id_path = "~/.ssh/id_container"
 
-    info_str = "session name: {}; src: {}; dest: {}; port: {}".format(
-                sess_name, src, dest, port)
-
+    info_str = "session name: {}; src: {}; dest: {}; port: {}".format(sess_name, src, dest, port)
     subprocess.Popen(["echo",
                       "\n****************\n{}; \n****************"
                       .format(info_str)], shell=False)
-
-    if not reverse:
-        scp_proc = subprocess.Popen(["scp", "-o", "StrictHostKeyChecking=no", "-i",
-                                    id_path, "-P", str(port), src, dest],
-                                    shell=False)
+    if r is True:
+        opt_r = "-r"
     else:
-        scp_proc = subprocess.Popen(["scp", "-o", "StrictHostKeyChecking=no", "-i",
-                                     id_path, "-P", str(port), dest, src],
-                                    shell=False)
+        opt_r = ""
+
+    scp_proc = subprocess.Popen(["scp", "-o", "StrictHostKeyChecking=no", "-i",
+                                 id_path, "-P", str(port), opt_r, src, dest], shell=False)
     scp_proc.communicate()
     print("Done")
     return 1
