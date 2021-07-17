fd5e6dd4c73beab224a7b5cade13fed80060968a
Joongi Kim <joongi@lablup.com>
2021-07-18 01:35:39 +0900

    refactor: Split check-group, check-scaling-group oommands and functions

--- src/ai/backend/client/cli/resource.py
+++ src/ai/backend/client/cli/resource.py
@@ -15,47 +15,48 @@ def resource():
 
 
 @resource.command()
-@click.argument('scaling_group', metavar='SCALING_GROUP', default="default")
-@click.argument('group', metavar='GROUP', default="default")
-@click.option('-a', '--all', 'all_', is_flag=True,
-              help="Get all resources of group.")
-def check(scaling_group: str, group: str, all_: bool) -> None:
+@click.argument('group', metavar='NAME', default="default")
+def check_group(group: str) -> None:
     """
-    Get available resources from the scaling groups.
+    Display the available resources from all allowed scaling groups of the given user group (project).
     """
     try:
         with Session() as session:
-            ret = session.Resource.get_available_resources(scaling_group, group)
+            ret = session.Resource.check_group(group)
             slot_types = session.Resource.get_resource_slots()
+            print("Limits:")
+            prefix = "- "
+            print_resource(ret["limits"], slot_types, prefix=prefix, nan_as_infinite=True)
+            print("Occupied:")
+            print_resource(ret["occupied"], slot_types, prefix=prefix)
+            print("Remaining:")
+            print_resource(ret["remaining"], slot_types, prefix=prefix)
+            print("Scaling groups allowed for this group:")
+            for sgroup_name in ret["scaling_groups"].keys():
+                print(f"  [{sgroup_name}]")
+                print("    Occupied:")
+                prefix = "    - "
+                print_resource(ret["scaling_groups"][sgroup_name]["occupied"], slot_types, prefix=prefix)
+                print("    Remaining:")
+                print_resource(ret["scaling_groups"][sgroup_name]["remaining"], slot_types, prefix=prefix)
+    except Exception as e:
+        print_error(e)
+        sys.exit(1)
 
+
+@resource.command()
+@click.argument('scaling_group', metavar='NAME', default="default")
+def check_scaling_group(scaling_group: str) -> None:
+    """
+    Display the available resource from the given scaling group.
+    """
+    try:
+        with Session() as session:
+            ret = session.Resource.check_scaling_group(scaling_group)
+            slot_types = session.Resource.get_resource_slots()
             print(f"Total remaining resources of scaling group [{scaling_group}]:")
             prefix = "- "
-            print_resource(ret["scaling_group_remaining"], slot_types, prefix=prefix, nan_as_infinite=True)
-
-            print("Each resources of scaling groups:")
-            if not all_:
-                print(f"  [{scaling_group}]")
-                print("    Using:")
-                prefix = "    - "
-                print_resource(ret["scaling_groups"][scaling_group]["using"], slot_types, prefix=prefix, nan_as_infinite=True)
-                print("    Remaining:")
-                print_resource(ret["scaling_groups"][scaling_group]["remaining"], slot_types, prefix=prefix, nan_as_infinite=True)
-            else:
-                for x in ret["scaling_groups"].keys():
-                    print(f"  [{x}]")
-                    print("    Using:")
-                    prefix = "    - "
-                    print_resource(ret["scaling_groups"][x]["using"], slot_types, prefix=prefix, nan_as_infinite=True)
-                    print("    Remaining:")
-                    print_resource(ret["scaling_groups"][x]["remaining"], slot_types, prefix=prefix, nan_as_infinite=True)
-
-            print("Group limits:")
-            prefix = "- "
-            print_resource(ret["group_limits"], slot_types, prefix=prefix, nan_as_infinite=True)
-            print("Group using:")
-            print_resource(ret["group_using"], slot_types, prefix=prefix, nan_as_infinite=True)
-            print("Group remaining:")
-            print_resource(ret["group_remaining"], slot_types, prefix=prefix, nan_as_infinite=True)
+            print_resource(ret["remaining"], slot_types, prefix=prefix)
     except Exception as e:
         print_error(e)
         sys.exit(1)
