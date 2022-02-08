7db225537a8c9e7f4d89196c8fd6aa6b10bd6b9d
Sergey Leksikov <lexikovs@gmail.com>
2022-02-08 16:56:12 +0900

    fix: wait untill filebrowser server initialized and responds

--- src/ai/backend/client/func/filebrowser.py
+++ src/ai/backend/client/func/filebrowser.py
@@ -1,3 +1,4 @@
+import asyncio
 import webbrowser
 
 from ..request import Request
@@ -14,6 +15,9 @@ class FileBrowser(BaseFunction):
         rqst.set_json({"vfolders": vfolders})
 
         async with rqst.fetch() as resp:
+            # give a grace period for filebrowser server to initialize and start
+            await asyncio.sleep(2)
+
             result = await resp.json()
             if result["status"] == "ok":
                 print(
@@ -24,6 +28,7 @@ class FileBrowser(BaseFunction):
                     URL: {result['addr']}
                     """,
                 )
+
                 webbrowser.open_new_tab(result["addr"])
             else:
                 raise Exception
