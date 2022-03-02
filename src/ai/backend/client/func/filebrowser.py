2b668d5d46f9aceaf1943db49f596311d3c8eec4
Sergey Leksikov <lexikovs@gmail.com>
2022-03-02 14:43:15 +0900

    fix: add error for exceeding max number of container

--- src/ai/backend/client/func/filebrowser.py
+++ src/ai/backend/client/func/filebrowser.py
@@ -10,12 +10,15 @@ class FileBrowser(BaseFunction):
     @api_function
     @classmethod
     async def create_or_update_browser(self, vfolders: list[str]):
-        rqst = Request("POST", "/browser/create")
+        rqst = Request("POST", "/storage/filebrowser/create")
         rqst.set_json({"vfolders": vfolders})
 
         async with rqst.fetch() as resp:
             result = await resp.json()
             if result["status"] == "ok":
+                if result['addr'] == "0":
+                    print("the number of container exceeds the maximum limit.")
+
                 print(
                     f"""
                     File Browser started.
@@ -31,7 +34,7 @@ class FileBrowser(BaseFunction):
     @api_function
     @classmethod
     async def destroy_browser(self, container_id):
-        rqst = Request("DELETE", "/browser/destroy")
+        rqst = Request("DELETE", "/storage/filebrowser/destroy")
         rqst.set_json({"container_id": container_id})
 
         async with rqst.fetch() as resp:
